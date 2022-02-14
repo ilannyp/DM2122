485fda9a2d8d760e2cfabeb59ce9e6be1c2ab53f
Rxyyxxn <rayyaanputra@gmail.com>
2022-02-14 14:36:29 +0800

    remove SP

--- Application/Source/Camera4.h
+++ Application/Source/Camera4.h
@@ -1,21 +1,21 @@
-#ifndef CAMERA_3_H
-#define CAMERA_3_H
+#ifndef CAMERA_4_H
+#define CAMERA_4_H
 
 #include "Camera.h"
 
-class Camera3 : public Camera
+class Camera4 : public Camera
 {
 public:
-	//Vector3 position;
-	//Vector3 target;
-	//Vector3 up;
+	Vector3 position;
+	Vector3 target;
+	Vector3 up;
 
 	Vector3 defaultPosition;
 	Vector3 defaultTarget;
 	Vector3 defaultUp;
 
-	Camera3();
-	~Camera3();
+	Camera4();
+	~Camera4();
 	virtual void Init(const Vector3& pos, const Vector3& target, const Vector3& up);
 	virtual void Update(double dt);
 	virtual void Reset();
